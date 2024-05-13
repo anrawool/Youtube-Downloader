c4d5cdfc641cab68dc591ba128e17da216949596
Sarthak <anrawool@gmail.com>
2024-05-13 18:50:20 +0530

    Name Fix

--- youtube_downloader/youtube_downloader/urls.py
+++ youtube_downloader/youtube_downloader/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for youtube_donwloader project.
+URL configuration for youtube_downloader project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
--- youtube_downloader/youtube_downloader/wsgi.py
+++ youtube_downloader/youtube_downloader/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for youtube_donwloader project.
+WSGI config for youtube_downloader project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "youtube_donwloader.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "youtube_downloader.settings")
 
 application = get_wsgi_application()
